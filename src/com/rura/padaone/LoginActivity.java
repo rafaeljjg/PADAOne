49f3317d28a13f7c389f34614a264a45d52366d0
rubenvk <rubenvk@gmail.com>
2013-01-03 13:40:36 +0100

    jjjj

--- src/com/rura/padaone/LoginActivity.java
+++ src/com/rura/padaone/LoginActivity.java
@@ -21,6 +21,7 @@ import android.widget.TextView;
  */
 public class LoginActivity extends Activity {
 	/**
+	 * hol
 	 * A dummy authentication store containing known user names and passwords.
 	 * TODO: remove after connecting to a real authentication system.
 	 */
